5b94110c5ac03e88a30326653c0d9c21c1a6e88d
MathTI <maxz_lima@hotmail.com>
2024-08-09 09:45:21 -0300

    Recuperando entradas do usuário e calculando resultado.

--- app/src/main/java/com/example/eletriccarapp/MainActivity.kt
+++ app/src/main/java/com/example/eletriccarapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.eletriccarapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var preco: EditText
+    private lateinit var kmPercorrido: EditText
+    private lateinit var btnCalcular: Button
+    private lateinit var tv_resultado: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setupView()
+        setupListeners()
+
     }
+
+    private fun setupView() {
+        preco = findViewById(R.id.et_preco)
+        kmPercorrido = findViewById(R.id.et_km_percorrido)
+        btnCalcular = findViewById(R.id.bCalcular)
+        tv_resultado = findViewById(R.id.tv_resultado)
+
+    }
+
+    private fun setupListeners() {
+        btnCalcular.setOnClickListener{
+            // Como queremos passar o valor digitado pelo usuário precisamos converter
+            // para string e para calcular precisamos converter para float
+            val preco = preco.text.toString().toFloat()
+            val km = kmPercorrido.text.toString().toFloat()
+            val resultado = preco / km
+            tv_resultado.text = resultado.toString()
+
+        }
+
+    }
+
+
 }
